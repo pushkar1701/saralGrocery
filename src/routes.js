b3dfaa4665f1bea0a106c47e99114100b643c062
karishma gupta <guptakarishma04@gmail.com>
2017-04-15 20:38:18 +0530

    added routing for other page

--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,45 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
     .state('app', {
       url: '/',
       component: 'app'
-    });
+    })
+    .state('wishlist', {
+            url: '/wishlist',
+            templateUrl: 'app/wishlist.html'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'app/about.html'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'app/register.html'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/login.html'
+        })
+        .state('shipping', {
+            url: '/shipping',
+            templateUrl: 'app/shipping.html'
+        })
+        .state('kitchen', {
+            url: '/kitchen',
+            templateUrl: 'app/kitchen.html'
+        })
+        .state('terms', {
+            url: '/terms',
+            templateUrl: 'app/terms.html'
+        })
+        .state('contact', {
+            url: '/contact',
+            templateUrl: 'app/contact.html'
+        })
+        .state('offer', {
+            url: '/offer',
+            templateUrl: 'app/offer.html'
+        })
+        .state('faqs', {
+            url: '/faqs',
+            templateUrl: 'app/faqs.html'
+        });
 }
